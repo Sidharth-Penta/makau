52418f76d0b3a80b7fb21458597de467ff7a03ca
Sidharth-Penta <pentasidharth1234@gmail.com>
2025-11-05 12:09:32 +0530

    Initial commit

---
+++ R/add.R
@@ -0,0 +1,16 @@
+#' Addition
+#'
+#' @param a first argument
+#' @param b second argument
+#'
+#' @returns a+b
+#' @export
+#'
+#' @examples
+#' 2+3
+#' 4+100
+#'
+add <- function(a, b)
+{
+  print(a+b)
+}
